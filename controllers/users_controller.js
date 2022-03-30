a5ab71cefeec25dbb536457c07ad34ed45e09df2
Shubham <shubham16290@gmail.com>
2022-03-30 16:00:16 +0530

    passport js setup

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,9 +3,21 @@ const User=require('../models/user')
 
 //profile page
 module.exports.profile = function(req,res){
-    res.render('user_profile',{
-        title:"profile"
-    })
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(user){
+                return res.render('user_profile',{
+                    title:'User Profile',
+                    user:user
+                })
+            }
+            return res.redirect('/users/sign-in');
+        });
+    }
+    else{
+        return res.redirect('/users/sign-in');
+    }
+    
 }
 
 //for sign
@@ -25,7 +37,7 @@ module.exports.signup = function(req,res){
 
 //for craeting profile
 module.exports.create = function(req,res){
-    //todo later
+ 
     if(req.body.password!=req.body.confirm_password){
         return res.redirect('back');
     }
@@ -34,6 +46,7 @@ module.exports.create = function(req,res){
             console.log('error in finding user in signing up');
             return;
         }
+        //if user not found then create new user
         if(!user){
             User.create(req.body,function(err,user){
                 if(err){
@@ -44,11 +57,51 @@ module.exports.create = function(req,res){
                 return  res.redirect('/users/sign-in');
             })
         }
+        //return to the sign up page
         else  return res.redirect('back');
     })
 }
 
-//for creating session after signing in
+//for creating session
 module.exports.createSession=function(req,res){
+    return res.redirect('/');
+}
 
+//for creating session after signing in
+// module.exports.createSession=function(req,res){
+    
+//     //steps to authenticate
+//     //find the user
+//     User.findOne({email:req.body.email},function(err,user){
+//         if(err){
+//             console.log('error in finding user in signing in');
+//             return;
+//         }
+//         //handle user found
+//         if(user){
+    
+//             //handle password which dont't match
+//             if(user.password!=req.body.password){
+//                 return res.redirect('back');
+//             }
+//             //handle session creation
+//             res.cookie('user_id',user.id);
+//             return res.redirect('/users/profile');
+//         }
+//         //handle user not found
+//         else return res.redirect('back');
+//     })
+
+    
+// }
+
+//signing out 
+module.exports.signout= function(req,res){
+
+    if(err){
+        console.log('error in finding user in signing in');
+        return;
+    }
+    
+    return res.redirect('/users/sign-in')
 }
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+
 const usersControlller = require('../controllers/users_controller');
 
-router.get('/profile',usersControlller.profile);
+router.get('/profile',passport.checkAuthentication, usersControlller.profile);
 router.get('/sign-in',usersControlller.signin);
 router.get('/sign-up',usersControlller.signup);
 
 router.post('/create',usersControlller.create);
+router.post('/create-session',usersControlller.createSession);
+//use passport as a middleware to authenticate
+router.post('/create-session',passport.authenticate(
+    'local',
+    {failureRedirect:'/users/sign-in'},
 
+),usersControlller.createSession)
+router.post('/sign-out',usersControlller.signout);
 module.exports=router;
